816a7cac8890a37b912074838276702b5c1d27e5
Mark Chen <dashtotherock@gmail.com>
2025-12-01 14:19:56 -0700

    add logs to folder

--- services/augustine-server/start.sh
+++ services/augustine-server/start.sh
@@ -3,4 +3,4 @@ set -e
 
 # Run from project root, not from the script's directory
 cd /app
-exec su -s /bin/bash apiuser -c "python3 /app/services/augustine-server/augustine_server.py"
+exec su -s /bin/bash apiuser -c "cd /app && python3 services/augustine-server/augustine_server.py"
